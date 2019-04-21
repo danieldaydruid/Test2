dbf9f5fa2fdb096c50e2b6ef8d757fc3e2e918d6
anonymous <mattktechie@gmail.com>
2019-04-21 08:24:30 -0700

    Test

---
+++ CS485Final/Assets/Scripts/PlayerStaffMovement.cs
@@ -0,0 +1,51 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStaffMovement : MonoBehaviour {
+	public GameObject _Player_control;
+	public float speed;
+	private float waitTime;
+	public float startWaitTime;
+
+	public Transform[] moveSpots;
+	private int randomSpot;
+	bool BoxCheck = false;
+	void Start() {
+		randomSpot = Random.Range(0, moveSpots.Length);
+	}
+
+	void Update() {
+		if(_Player_control.GetComponent<Player_control>().PlayerIsInteractingWithBox())
+		{
+			//transform.position = _Player_control.GetComponent<Player_control>().transform.position + new Vector3(0.0f, 1.4f, 0.8f);
+			transform.position = GameObject.Find("/Player/WeaponSheath").transform.position;
+			transform.rotation = GameObject.Find("/Player/WeaponSheath").transform.rotation;
+			BoxCheck = true;
+		}
+		else
+		{
+			if (BoxCheck == true){
+				transform.position = GameObject.Find("/Player/WeaponOut").transform.position;
+				transform.rotation = GameObject.Find("/Player/WeaponOut").transform.rotation;
+				BoxCheck = false;
+			}
+			if (_Player_control.GetComponent<Player_control>().PlayerIsMoving())
+			{
+				//Can un-comment this if you want the staff to spin around in a circle,
+				//or you can modify the 3 float values (x, y, z) to make it do rotate other ways.
+				//transform.Rotate(10f, 0f, 10f, Space.Self);
+				transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+				if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f){
+					if(waitTime <= 0) {
+						randomSpot = Random.Range(0, moveSpots.Length);
+						waitTime = startWaitTime;
+					}
+					else {
+						waitTime -= Time.deltaTime;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ CS485Final/Assets/Scripts/Player_control.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_control : MonoBehaviour {
+	public GameObject PushBox;
+	public int count = 0;
+	public bool cursor_check = false;
+	public float speed = 6.0F;
+	public float gravity = 20.0F;
+	public Vector3 curPos;
+	public Vector3 lastPos;
+	private Vector3 moveDirection = Vector3.zero;
+	public CharacterController controller;
+	public GameObject RaycastObject;
+
+	void Start() {
+	// Store reference to attached component
+		controller = GetComponent<CharacterController>();
+		PushBox = GameObject.Find("PushBox");
+		RaycastObject = GameObject.Find("/Player/RaycastObject");
+	}
+
+	void Update() {
+		PlayerIsMoving();
+		PlayerIsInteractingWithBox();
+		if(Input.GetKey(KeyCode.Escape)) {
+				cursor_check = false;
+			}
+			if(Input.GetKey(KeyCode.Space)) {
+				cursor_check = true;
+			}
+			if (cursor_check) {
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+			else {
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+		if(controller.transform.position.y >= 10 || controller.transform.position.y <= -10) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		}
+		//Load Level 1
+		if(controller.transform.position.x > 5 && controller.transform.position.z <= -51) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}//Shift + Ctrl + P to pause the game if your mouse gets stuck
+		if(controller.transform.position.x <= -18 && controller.transform.position.z >= 3) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+		}
+	// Character is on ground (built-in functionality of Character Controller)
+	if (controller.isGrounded) {
+			if(Input.GetKey(KeyCode.Escape)) {
+				cursor_check = false;
+			}
+			if(Input.GetKey(KeyCode.Space)) {
+				cursor_check = true;
+			}
+			if (cursor_check) {
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+			else {
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+			float rotLeftRight = Input.GetAxis("Mouse X");
+			transform.Rotate(0, rotLeftRight, 0);
+			
+	// Use input up and down for direction, multiplied by speed
+	moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+	moveDirection *= speed;
+
+	}
+	// Apply gravity manually.
+	moveDirection.y -= gravity * Time.deltaTime;
+	// Move Character Controller
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count++;
+		}
+		if (count == 1) {
+			Destroy(GameObject.Find("ZombieCube"));
+		}
+		if (count == 2) {
+			Destroy(GameObject.Find("ZombieCube_1"));
+		}
+		if (count == 3) {
+			Destroy(GameObject.Find("ZombieCube_2"));
+		}
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.name == "PushBox")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+	public bool PlayerIsMoving()
+	{
+		bool temp = false;
+		curPos = controller.transform.position;
+		if(curPos == lastPos) temp = false;
+		else temp = true;
+		lastPos = curPos;
+		return temp;
+	}
+	public bool PlayerIsInteractingWithBox()
+	{
+		RaycastHit hit;
+		Vector3 fwd = RaycastObject.transform.TransformDirection(Vector3.forward);
+		if (Physics.Raycast(RaycastObject.transform.position, fwd, out hit, 1))
+		{
+			if(hit.transform.gameObject.name == "PushBox")
+			{
+				if(Input.GetMouseButton(0)) {
+					PushBox.transform.parent = transform;
+					return true;
+				}
+				if(Input.GetMouseButtonUp(0)) {
+					PushBox.transform.parent = null;
+					return false;
+				}
+			}
+		}
+		/*if(Vector3.Distance(PushBox.transform.position, transform.position) <= 2.0f )
+		{
+			if(Input.GetMouseButton(0)) {
+				PushBox.transform.parent = transform;
+				return true;
+			}
+			if(Input.GetMouseButtonUp(0)) {
+				PushBox.transform.parent = null;
+				return false;
+			}
+		}*/
+		return false;
+	}
+}
